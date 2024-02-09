543702cea1ae8a789ce2b9ebfb051adbe6fc790e
jemmyblair <42910234+jamesmhee@users.noreply.github.com>
2024-02-09 16:40:17 +0700

    de

--- src/main/java/com/levo/dockerexample/models/UserRepository.java
+++
@@ -1,8 +0,0 @@
-// package com.levo.dockerexample.models;
-
-// import com.levo.dockerexample.models.Users;
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-
-// public interface UserRepository extends JpaRepository<Users, Integer> {
-// }
